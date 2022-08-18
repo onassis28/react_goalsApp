52028054e65b41ce402d4db3f287e8d20a1fbe11
onassis28 <onassis.sowah@gmail.com>
2022-08-19 00:15:58 +0200

    changes to environment file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello Worl');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+
+const testing = 2000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
